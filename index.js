6d07a4ab3028a44eb407f71c284de6ddb66338c5
Jakob Fix <jakob.fix@gmail.com>
2019-10-28 23:44:16 +0100

    Rewrite script to permit configuration via JSON files for each publications, multlingual data and separation of publication data and the language-specific blurbs (intro and outro). For the time being we have English and French, other languages can now quite easily be added when needed.

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const argv = require('yargs').argv
+const audioConfigFile  = path.join(__dirname, argv._[0])
+
+if (!fs.existsSync(audioConfigFile)) {
+    throw Error(`Audio config file not found, check your input: ${audioConfigFile}`)
+}
 const createAudio = require('./createAudioGoogle')
 const concatAudio = require('./concatAudio')
+const langConfig = require('./lang-config.json')
 
-const pubId = "abc123"
-const pubTitle = "OECD Economic Surveys: Colombia 2019"
-const pubDate = "2019-10-24"
-const pubAbstract = `Colombia has made good economic and social progress over the last two decades. Macroeconomic policies are solid and have sustained growth and smooth adjustments to shocks over the years. Maintaining and strengthening the policy framework is key to sustainable macroeconomic policies and setting the basis for higher productivity and inclusiveness. Putting Colombia on a path to stronger and more inclusive growth, and reducing dependence on natural resources, requires boosting productivity by adopting structural reforms in competition, regulations, trade policy, infrastructure, innovation, and skills. Reducing informality and boosting job-quality would extend the benefits of growth to all Colombians, underpinning economic and political support for reform. SPECIAL FEATURES: BOOSTING EXPORTS AND INTEGRATION INTO THE WORLD ECONOMY; FOSTERING HIGH-QUALITY JOBS FOR ALL`
+const audioConfig = JSON.parse(fs.readFileSync(argv._[0]))
 
-const text1 = `<speak><emphasis level="moderate">Hello!</emphasis> <break time="800ms"/> The following is an audio abstract for "${pubTitle}"<break time="1s"/></speak>`
-const text2 = `<speak><prosody volume="+3dB">${pubAbstract}</prosody></speak>`
-const text3 =  `<speak><break time="1s"/>You've been listening to an audio abstract of "${pubTitle}" which you can read in its entirety on OECD i-Library as of <say-as interpret-as="date" format="yyyy-mm-dd" detail="1">${pubDate}</say-as>. <break time="1s"/> If you don't mind, we'd like you to tell us what you think of this new audio abstract service. <break time="1s"/>You find the link to a short survey in the track description. <break time="1s"/><emphasis level="moderate">Thank you!</emphasis></speak>`;
+audioConfig.forEach(item => {
+    const pubId = item.id
+    const pubTitle = item.title
+    const pubDate = item.date
+    const pubAbstract = item.abstract
+    const pubLanguage = item.lang
+    const langCfg = langConfig[pubLanguage]
 
-Promise.all([
-createAudio(text1, `1-${pubId}.mp3`, "ssml", {languageCode: 'en-GB', name: 'en-GB-Wavenet-A', ssmlGender: 'FEMALE'}),
-createAudio(text2, `2-${pubId}.mp3`, "ssml", {languageCode: 'en-GB', name:  'en-GB-Wavenet-B', ssmlGender: 'MALE'}),
-createAudio(text3, `3-${pubId}.mp3`, "ssml", {languageCode: 'en-GB', name: 'en-GB-Wavenet-A', ssmlGender: 'FEMALE'})
-]).then(() => {
-    concatAudio([`1-${pubId}.mp3`, `2-${pubId}.mp3`, `3-${pubId}.mp3`], `0-${pubId}.mp3`)
+    const intro = langCfg.intro.start.replace('${pubTitle}', pubTitle)
+    const abstract = `<speak><prosody rate="90%" volume="+3dB">${pubAbstract}</prosody></speak>`
+    const outro =  langCfg.intro.end
+        .replace('${pubTitle}', pubTitle)
+        .replace('${pubDate}', pubDate)
+
+    // console.log(intro, abstract, outro)
+    Promise.all([
+        createAudio(intro, `1-${pubId}.mp3`, "ssml", {
+            languageCode: langCfg.languageCode,
+            name: langCfg.intro.languageName,
+            ssmlGender: langCfg.intro.ssmlGender
+        }),
+        createAudio(abstract, `2-${pubId}.mp3`, "ssml", {
+            languageCode: langCfg.languageCode, 
+            name:  langCfg.abstract.languageName,
+            ssmlGender: langCfg.abstract.ssmlGender
+        }),
+        createAudio(outro, `3-${pubId}.mp3`, "ssml", {
+            languageCode: langCfg.languageCode,
+            name: langCfg.intro.languageName,
+            ssmlGender: langCfg.intro.ssmlGender
+        })
+    ]).then(() => {
+        concatAudio([`1-${pubId}.mp3`, `2-${pubId}.mp3`, `3-${pubId}.mp3`], `0-${pubId}.mp3`)
+    })
 })
